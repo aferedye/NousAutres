b5718eaa1828d20d10d1a986e4ec06479aa90208
NousAutres <aferedye@gmail.com>
2025-06-28 03:03:04 +0200

    Page d'accueil ok / Ajout charte & manifeste / Ajout route dans le controleur / Ajout des pages contenu dans le menu / Premier prototype de la page arbre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,41 @@ final class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/manifeste', name: 'manifeste')]
+    public function manifeste(): Response
+    {
+        return $this->render('manifeste.html.twig');
+    }
+
+    #[Route('/charte', name: 'charte')]
+    public function charte(): Response
+    {
+        return $this->render('charte.html.twig');
+    }
+
+    #[Route('/cercles', name: 'cercles')]
+    public function cercles(): Response
+    {
+        return $this->render('cercles.html.twig');
+    }
+
+    #[Route('/projets', name: 'projets')]
+    public function projets(): Response
+    {
+        return $this->render('projets.html.twig');
+    }
+
+    #[Route('/creer-cercle', name: 'creer_cercle')]
+    public function creerCercle(): Response
+    {
+        return $this->render('creer_cercle.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
 }
